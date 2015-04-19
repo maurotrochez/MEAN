9290426fc87c0397e81a0771bc190ed75c183d19
David <maurotrochez@gmail.com>
2015-04-19 12:59:04 -0500

    Add express part

--- config/express.js
+++ config/express.js
@@ -1,8 +1,10 @@
+var config = require('./config');
 var express = require('express');
 var morgan = require('morgan');
 var compress = require('compression');
 var bodyParser = require('body-parser');
-var methoOverride = require('method-override');
+var methodOverride = require('method-override');
+var session = require('express-session');
 
 module.exports = function(){
     var app = express();
@@ -17,9 +19,20 @@ module.exports = function(){
         extended: true
     }));
     app.use(bodyParser.json());
-    app.use(methoOverride());
+    app.use(methodOverride());
+
+	app.use(session({
+		saveUninitialized: true,
+		resave: true,
+		secret: config.sessionSecret
+	}));
+
+	app.set('views', './app/views');
+	app.set('view engine', 'ejs');
 
     require('../app/routes/index.server.routes.js')(app);
+
+	app.use(express.static('./public'));
 
     return app;
 };
