c8ddaa9909748d5877135deac554791723273613
David Trochez <maurotrochez@gmail.com>
2015-05-01 16:22:14 -0500

    Add local authenticate to model User

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var crypto = require('crypto');
 
 var UserSchema = new Schema({
 	firstName: String,
@@ -7,23 +8,32 @@ var UserSchema = new Schema({
 	email: {
 		type:String,
 		index: true,
-		match: /.+\@.+\..+/
+		match: [/.+\@.+\..+/, 'Please fill a valid e-mail address']
 	},
 	username: {
 		type: String,
 		trim: true,
 		unique: true,
-		required: true
+		required: 'Username is required'
 	},
 	password: {
 		type: String,
 		validate: [
 			function (password) {
-				return password.length >= 6;
+				return password && password.length > 6;
 			},
 			'Password should be longer'
 		]
 	},
+	salt:{
+		type: String
+	},
+	provider: {
+		type: String,
+		required: 'Provider is required'
+	},
+	providerId: String,
+	proverData: {},
 	role: {
 		type: String,
 		enum: ['Admin', 'Owner', 'User']
@@ -62,12 +72,41 @@ UserSchema.virtual('fullName')
 		this.lastName = splitName[1] || '';
 	});
 
+UserSchema.pre('save', function (next) {
+	if (this.password){
+		this.salt = new Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+		this.password = this.hashPassword(this.password);
+	}
+	next();
+});
+
+UserSchema.methods.hashPassword = function (password) {
+	return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
+};
+
 UserSchema.statics.findOneByUsername = function (username, callback) {
 	this.findOne({ username: new RegExp(username, 'i') }, callback);
 };
 
+UserSchema.statics.findUniqueUsername = function (username, suffix, callback) {
+	var _this = this;
+	var possibleUsername = username + (suffix || '');
+
+	_this.findOne({ username: possibleUsername }, function (err, user) {
+		if(err){
+			if(!user){
+				callback(possibleUsername);
+			}else{
+				return _this.findUniqueUsername(username, (suffix || 0) + 1, callback);
+			}
+		}else{
+			callback(null);
+		}
+	});
+};
+
 UserSchema.methods.authenticate = function (password) {
-	return this.password === password;
+	return this.password === this.hashPassword(password);
 };
 
 UserSchema.post('save', function (next) {
