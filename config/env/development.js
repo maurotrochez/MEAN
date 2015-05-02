4085ed9ed3f8fad60e66a26f0c382b211f283620
David Trochez <maurotrochez@gmail.com>
2015-05-02 18:34:06 -0500

    Add passport's facebook strategy

--- config/env/development.js
+++ config/env/development.js
@@ -1,4 +1,9 @@
 module.exports = {
 	db: 'mongodb://localhost/mean',
-	sessionSecret: 'developmentSessionSecret'
+	sessionSecret: 'developmentSessionSecret',
+	facebook: {
+		clientId: '1659578757606857',
+		clientSecret: 'b2466ce344e22257177b114981a23cdd',
+		callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+	}
 };
