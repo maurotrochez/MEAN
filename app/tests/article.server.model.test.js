518e5b74fabd5ebcf268f29f3cd7179a66b2e2d8
David Trochez <maurotrochez@gmail.com>
2015-05-24 17:32:00 -0500

    Add test for article model and controller

---
+++ app/tests/article.server.model.test.js
@@ -0,0 +1,52 @@
+var app = require('../../server.js');
+var should = require('should');
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+var Article = mongoose.model('Article');
+
+var user, article;
+
+describe('Article model unit tests:', function () {
+	beforeEach(function (done) {
+		console.log(article);
+		user = new User({
+			firstName: 'Full',
+			lastName: 'Name',
+			displayName: 'Full Name',
+			email: 'test@test.com',
+			username: 'username',
+			password: 'password'
+		});
+
+		user.save(function () {
+			article = new Article({
+				title: 'Article Title',
+				content: 'Article content',
+				user: user
+			});
+
+			done();
+		});
+	});
+
+	describe('Testing the save method', function () {
+		it('Should be able to save without problems', function () {
+			article.save(function (err) {
+				should.not.exist(err);
+			});
+		});
+
+		it('Should not be able to save an article without a title', function () {
+			article.title = '';
+
+			article.save(function (err) {
+				should.exist(err);
+			});
+		});
+	});
+
+	afterEach(function (done) {
+		Article.remove().exec();
+		done();
+	});
+});
---
+++ app/tests/articles.server.controller.tests.js
@@ -0,0 +1,65 @@
+var app = require('../../server');
+var request = require('supertest');
+var should = require('should');
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+var Article = mongoose.model('Article');
+
+var user, article;
+
+describe('Articles controller unit test:', function () {
+	beforeEach(function (done) {
+		user = new User({
+			firstName: 'Full',
+			lastName: 'Name',
+			displayName: 'Full Name',
+			email: 'test@test.com',
+			username: 'username',
+			password: 'password'
+		});
+
+		user.save(function () {
+			article = new Article({
+				title: 'Article title',
+				content: 'Article content',
+				user: user
+			});
+
+			article.save(function (err) {
+				done();
+			});
+		});
+	});
+	describe('Testing the GET methods', function () {
+		it('Should be able to get the list of articles', function (done) {
+			request(app).get('/api/articles/')
+				.set('Accept', 'application/json')
+				.expect('Content-Type', '/json/')
+				.expect(200)
+				.end(function (err, res) {
+					res.body.should.be.an.Array.and.have.lengthOf(1);
+					res.body[0].should.have.property('title', article.title);
+					res.body[0].should.have.property('content', article.content);
+					done();
+				});
+		});
+
+		it('Should be able to get the specific article', function (done) {
+			request(app).get('/api/articles/' + article.id)
+				.set('Accept', 'application/json')
+				.expect('Content-Type', '/json/')
+				.expect(200)
+				.end(function (err, res) {
+					res.body.should.be.an.Object.and.have.property('title', article.title);
+					res.body.should.have.property('content', article.content);
+					done();
+				});
+		});
+	});
+
+	afterEach(function (done) {
+		Article.remove().exec();
+		User.remove().exec();
+		done();
+	});
+});
