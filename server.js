740148d789abd0373d69268d94406b6ee551205e
David Trochez <maurotrochez@gmail.com>
2015-05-31 17:04:32 -0500

    Add AngularJS E2E testing

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
+console.log(process.env.NODE_ENV);
 var mongoose = require('./config/mongoose');
 var express = require('./config/express');
 var passport = require('./config/passport');
