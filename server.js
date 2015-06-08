4a75264bf6de5846f3b8e9ec71854dc271e2a85b
David Trochez <maurotrochez@gmail.com>
2015-06-08 17:40:50 -0500

    Modifed for deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 console.log(process.env.NODE_ENV);
 
 var mongoose = require('./config/mongoose');
@@ -16,4 +16,4 @@ app.listen(server_port, server_ip_address);
 
 module.exports = app;
 
-console.log('Server running at localhost:3000');
+console.log('Server running at '+server_ip_address+':'+server_port);
