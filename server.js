b41b344e016c4a1eebf0fb690584cc89cda1b2e6
David Trochez <maurotrochez@gmail.com>
2015-06-08 00:46:17 -0500

    Version 0.0.1

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 console.log(process.env.NODE_ENV);
+
 var mongoose = require('./config/mongoose');
 var express = require('./config/express');
 var passport = require('./config/passport');
@@ -8,7 +12,8 @@ var db = mongoose();
 var app = express(db);
 var passport = passport();
 
-app.listen(3000);
+app.listen(server_port, server_ip_address);
+
 module.exports = app;
 
 console.log('Server running at localhost:3000');
